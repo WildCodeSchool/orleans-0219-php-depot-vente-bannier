ac6ee8b175829a5fad982913f6cda6e732386823
Vinny <paulvinny@gmail.com>
2019-04-17 15:35:58 +0200

    add AdminProduct controller and manager

---
+++ src/Controller/AdminProductController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+use App\Model\ProductManager;
+
+class AdminProductController extends AbstractController
+{
+
+    /**
+     * Display products administration page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function list()
+    {
+        $productManager = new ProductManager();
+        $products = $productManager -> showAllWithJoin();
+        return $this->twig->render('Admin/products.html.twig', ['products' => $products]);
+    }
+}
---
+++ src/Model/ProductManager.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ *
+ */
+class ProductManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'product';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    /**
+     * Get all row from all the tables with join.
+     *
+     * @return array
+     */
+    public function showAllWithJoin(): array
+    {
+        return $this->pdo->query("SELECT product.id, product.name, product.price, product.date_added, 
+product.date_saled, ahead, category.name as categories FROM $this->table inner join bannier.category 
+on product.categories_id = category.id order by category.name ASC, product.name ASC;")->fetchAll();
+    }
+}
