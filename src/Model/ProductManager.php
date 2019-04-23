a67e1d41af88659ce6506933bf68a6988d0a2773
Vinny <paulvinny@gmail.com>
2019-04-23 15:12:32 +0200

    add insert method for ProductManager class

---
+++ src/Model/ProductManager.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+use utils\CleanData;
+
+/**
+ *
+ */
+class ProductManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'product';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    /**
+     * Get all row from all the tables with join.
+     *
+     * @return array
+     */
+    public function showAllWithCategories(): array
+    {
+        $query = "SELECT product.id, product.name, product.price, product.date_added, product.date_saled, ahead
+                    ,category.name AS categories FROM $this->table 
+                    INNER JOIN bannier.category 
+                    ON product.categories_id = category.id 
+                    ORDER BY category.name ASC, product.name ASC;";
+        return $this->pdo->query($query)->fetchAll();
+    }
+
+    /**
+     *
+     * Insert new products in table
+     *
+     * @param array $item
+     * @return int
+     */
+    public function insert(array $data): int
+    {
+        $cleanData = new CleanData($data);
+
+
+        $query = "INSERT INTO $this->table 
+                (`name`, `categories_id`, `description`, `price`, 
+                `date_added`, `ahead`)
+                VALUES (:name, :categories_id, :description, :price, 
+                :date_added, :ahead)";
+
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('name', $data['name'], \PDO::PARAM_STR);
+        $statement->bindValue('categories_id', $data['categories_id'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $data['description'], \PDO::PARAM_STR);
+        $statement->bindValue('price', $data['price'], \PDO::PARAM_STR);
+        $statement->bindValue('date_added', $data['date_added'], \PDO::PARAM_STR);
+        $statement->bindValue('ahead', $data['ahead'], \PDO::PARAM_STR);
+
+        if ($statement->execute()) {
+            $id = (int)$this->pdo->lastInsertId();
+        }
+    }
+}
