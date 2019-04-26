41636327c667d1d4614d93c9a003e9f1bd20e4b7
Jessica Salvat <jsalvat7@gmail.com>
2019-04-26 11:37:54 +0200

    Ajout de la faq

---
+++ src/Controller/FaqController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\CategoryManager;
+use App\Model\ProductManager;
+class FaqController extends AbstractController
+{
+
+    public function faq()
+    {
+        $categoryManager = new CategoryManager();
+        $categories = $categoryManager -> selectAll();
+
+        $productManager = new ProductManager();
+        $products= $productManager->showAhead();
+
+        return $this->twig->render('Faq/index.html.twig', ['categories' => $categories,
+            'products' => $products]);
+    }
+}
+
+
